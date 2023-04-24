9da0001c73c081b1b15e4b5223a09cd8d4b8cb16
Pankaj1105 <98206434+Pankaj1105@users.noreply.github.com>
2023-04-24 21:18:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,45 +1,21 @@
-//your JS code here. If required.
-const sounds = [];
+let appl = document.getElementById("appl");
+let boo = document.getElementById("boo");
+let gasp = document.getElementById("gasp");
+let tada = document.getElementById("tada");
+let vic = document.getElementById("vic");
+let wrg = document.getElementById("wrg");
+let aud = document.getElementById("audio");
 
-for (let i = 1; i <= 6; i++) {
-  sounds.push(new Promise((resolve, reject) => {
-    const sound = new Audio(`sounds/audio${i}.mp3`);
-    sound.addEventListener('canplay', () => {
-      resolve(sound);
-    });
-    sound.addEventListener('error', (error) => {
-      reject(error);
-    });
-  }));
+appl.addEventListener('click', myFunc);
+boo.addEventListener('click', myFunc);
+gasp.addEventListener('click', myFunc);
+tada.addEventListener('click', myFunc);
+vic.addEventListener('click', myFunc);
+wrg.addEventListener('click', myFunc);
+
+function myFunc() {
+	aud.play();
 }
-
-Promise.all(sounds)
-  .then((loadedSounds) => {
-    const buttons = document.querySelectorAll('.btn');
-    const stopButton = document.querySelector('.stop');
-
-    function playSound(index) {
-      stopSounds();
-      loadedSounds[index].play();
-    }
-
-    function stopSounds() {
-      loadedSounds.forEach((sound) => {
-        sound.pause();
-        sound.currentTime = 0;
-      });
-    }
-
-    buttons.forEach((button, index) => {
-      button.addEventListener('click', () => {
-        playSound(index);
-      });
-    });
-
-    stopButton.addEventListener('click', () => {
-      stopSounds();
-    });
-  })
-  .catch((error) => {
-    console.error('Failed to load sounds', error);
-  });
+function pause() {
+	aud.pause();	
+}
