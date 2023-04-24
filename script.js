ce71520a0572da030eb257fa4a7a28061bc81172
Pankaj1105 <98206434+Pankaj1105@users.noreply.github.com>
2023-04-24 21:24:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,11 @@
 //your JS code here. If required.
-let appl = document.getElementById("appl");
-let boo = document.getElementById("boo");
-let gasp = document.getElementById("gasp");
-let tada = document.getElementById("tada");
-let vic = document.getElementById("vic");
-let wrg = document.getElementById("wrg");
-let aud = document.getElementById("audio");
+function play() {
+    var audio = document.getElementById("audio");
+    audio.play();
+  }
 
-appl.addEventListener('click', myFunc);
-boo.addEventListener('click', myFunc);
-gasp.addEventListener('click', myFunc);
-tada.addEventListener('click', myFunc);
-vic.addEventListener('click', myFunc);
-wrg.addEventListener('click', myFunc);
-
-function myFunc() {
-	aud.play();
-}
-function pause() {
-	aud.pause();	
+function stop() {
+  var audio = document.getElementById("audio");
+  audio.pause();
+  audio.currentTime = 0;
 }
